4385efe03fab7d4e67085ecfa8269134733a5e90
Daniel Popescu <same.dan@gmail.com>
2021-04-08 17:53:41 +0200

    started frontend

---
+++ frontend/src/components/Generation.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Generation extends Component {
+  render() {
+    const generation = { generationId: 999, expiration: "2022-05-01" };
+    return (
+      <div>
+        <h3>Generation {generation.generationId}. Expires on:</h3>
+        <h4>{new Date(generation.expiration).toString()}</h4>
+      </div>
+    );
+  }
+}
+
+export default Generation;
